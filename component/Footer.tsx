1118dcaf722dcb4d322b3939c641b2f476f2f473
Tech Pidgin <65148346+Techpidgin@users.noreply.github.com>
2023-10-28 14:55:47 +0300

    Update Footer.tsx

--- component/Footer.tsx
+++ component/Footer.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
 
 function Footer() {
   return (
     <footer>
-     <section className={styles.footer}>
-            
-              <h5 className={styles.stakingInfo1}><img
-                src="logo.png"
-                alt=""
-               style={{ width: '50px', height: '50px' }}
-              />{' '}© 2023 GAMBLE-FI IS BASED . All rights reserved. | Brand Assets | TVL :   1,000,000 ETHER IS FUD</h5>
-    </section>
+  <section className={styles.footer}>
+  <h5 className={styles.stakingInfo1}>
+    <img src="Logo.png" alt="" style={{ width: '50px', height: '50px' }} />{' '}
+    © 2023 GAMBLE-FI IS BASED. All rights reserved. | Brand Assets | TVL: 1,000,000 ETHER IS FUD{' '}
+    <span className={styles.linkContainer}>
+      {/* Add TELEGRAM link with an icon */}
+      <Link legacyBehavior href="https://t.me/+Uklr9zsuEJ5lODc0">
+        <a>
+          <img src="/tg.png" className={styles.linkIcon} />
+        </a>
+      </Link>
+      <Link legacyBehavior href="https://twitter.com/GIBISBIG">
+        <a>
+          <img src="/x.png" className={styles.linkIcon} />
+        </a>
+      </Link>
+    </span>
+  </h5>
+</section>
+
    
             
  
