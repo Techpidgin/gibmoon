9d8f2ef284c339e135613c879df324ae340e0f8e
Tech Pidgin <65148346+Techpidgin@users.noreply.github.com>
2023-11-12 20:45:58 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,16 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+       <div className={styles.overlay}></div>
       <Header />
       <section>
         <h5>
-          <span className={styles.mySpan}>DEFLATIONARY/.</span>
+          <span className={styles.mySpan}>100% ON-CHAIN BETTING</span>
 
-          <span className={styles.mySpan1}>GAMBLEFI IS BASED /.</span>
+          <span className={styles.mySpan1}>GAMBLEFI IS BASED.</span>
         </h5>
-        
         <Link legacyBehavior href="/claim">
-          <button className={styles.button}>🚀</button>
+          <button className={styles.button}>🔥</button>
         </Link>
       </section>
       <Footer />
